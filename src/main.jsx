01da699472973e43df58afd4114862db3eeec26f
LeandroSouza82 <Lsouza557@gmail.com>
2026-02-17 15:01:34 -0300

    style: mapa em tela cheia (100vh)

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MapaLogistica />
+    <MapaLogistica height="100vh" />
   </React.StrictMode>
 );
