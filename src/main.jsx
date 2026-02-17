e79e2d93e4450f5bb670bcd449e9ef0449786e92
LeandroSouza82 <Lsouza557@gmail.com>
2026-02-17 15:00:32 -0300

    chore(main): renderizar MapaLogistica diretamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DriverApp from './DriverApp.jsx';
+import MapaLogistica from './MapaLogistica.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DriverApp />
+    <MapaLogistica />
   </React.StrictMode>
 );
